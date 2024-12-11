0560c2f8bbb9b07729c1827c23811cfb26e48f63
saad moustafa <saadmoustafa87@outlook.com>
2024-12-11 02:28:00 +0200

    C42-G03-C#02

--- First project/Program.cs
+++ First project/Program.cs
@@ -7,61 +7,54 @@ namespace First_project
         static void Main(string[] args)
         {
 
-            /* #region Program01
+            #region Write a program that allows the user to enter a number then print it.
 
-             1-Write a program that allows the user to enter a number then print it.
 
-                -- ANSWER ==>
+            /*
                Console.WriteLine("Enter the Nums AS U Want");
 
                int Nums = Convert.ToInt32(Console.ReadLine());
 
                Console.WriteLine("The Nums You Entered is: " + Nums);
-               #endregion
             */
+            #endregion
+
+            #region   2-Write C# program that Assigning one value type variable  to another and modifying the value of one variable and  mention what will happen
 
 
-            /*  #region program02
+            /*
+           int a = 10;
+
+           int b = a;
+
+           Console.WriteLine($"Initial values: a = {a}, b = {b}");
+
+           // Modify the value of 'a'
+           a = 20;
+           Console.WriteLine($"After modification: a = {a}, b = {b}");
+            */
+            #endregion
+
+            #region 3-Write C# program that Assigning one reference type variable to another and   modifying the object through one variable and mention what will happen
+
+
+
+            /*
+                         String person1 = "Alice";
+
+                         String person2 = person1;
+
+                         // Print the initial names
+                         Console.WriteLine($"Initial names: person1.Name = {person1}, person2.Name = {person2}");
+
+                         // Modify the object through person1
+                         person1 = "Bob";
+
+                         // Print the names after modification
+                         Console.WriteLine($"After modification: person1.Name = {person1}, person2.Name = {person2}");
+              */
+            #endregion
             
-            2-Write C# program that Assigning one value type variable 
-            to another and modifying the value of one variable and 
-            mention what will happen
-
-            The Answer ==>
-
-              int a = 10;
-
-              int b = a;
-
-              Console.WriteLine($"Initial values: a = {a}, b = {b}");
-
-              // Modify the value of 'a'
-              a = 20;
-              Console.WriteLine($"After modification: a = {a}, b = {b}");
-              #endregion
-            */
-
-            /* #region program03
-             
-            3-Write C# program that Assigning one reference type variable to another and 
-            modifying the object through one variable and mention what will happen
-
-            The Answer ==>
-
-             String person1 = "Alice";
-
-             String person2 = person1;
-
-             // Print the initial names
-             Console.WriteLine($"Initial names: person1.Name = {person1}, person2.Name = {person2}");
-
-             // Modify the object through person1
-             person1 = "Bob";
-
-             // Print the names after modification
-             Console.WriteLine($"After modification: person1.Name = {person1}, person2.Name = {person2}");
-             #endregion
-             */
 
 
         }
